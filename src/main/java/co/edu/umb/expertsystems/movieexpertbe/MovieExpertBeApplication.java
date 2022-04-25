b95629792b34872e48db5092fd8d82f88e2ea099
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2022-04-24 20:50:48 -0500

    first commit

---
+++ src/main/java/co/edu/umb/expertsystems/movieexpertbe/MovieExpertBeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umb.expertsystems.movieexpertbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieExpertBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieExpertBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umb/expertsystems/movieexpertbe/MovieExpertBeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umb.expertsystems.movieexpertbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieExpertBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
